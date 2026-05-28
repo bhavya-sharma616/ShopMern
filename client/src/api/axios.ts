99f9b2917e1a700033eb6c7142806d9d6946c4d2
Bhavya Sharma <bhavya3720@gmail.com>
2026-05-28 18:29:46 +0530

    added frontend env

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_APP_BASE_URL || "http://localhost:5000/api/v1",
 });
 
 API.interceptors.request.use(
