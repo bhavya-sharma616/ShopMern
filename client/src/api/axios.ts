1881a07f75ad2b82d9ada31ac0c80db4279b6080
Bhavya Sharma <bhavya3720@gmail.com>
2026-05-29 10:47:02 +0530

    axios.ts

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL:"http://localhost:5000/api/v1",
-  baseURL:import.meta.env.VITE_APP_BASE_URL,
+  baseURL:import.meta.env.VITE_APP_URL,
 });
 
 API.interceptors.request.use(
