de4d322e6877bcffcdfc5a72ef2748a0872b9dd5
Bhavya Sharma <bhavya3720@gmail.com>
2026-05-29 15:38:23 +0530

    changed baseUrl

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-<<<<<<< HEAD
   baseURL: import.meta.env.VITE_APP_BASE_URL || "http://localhost:5000/api/v1",
-=======
-  // baseURL:"http://localhost:5000/api/v1",
-  
-  baseURL: "https://shopmern-1c1d.onrender.com/api/v1",
->>>>>>> a600036f1ab813c77e853d3c64573163ec88e775
 });
 
 
